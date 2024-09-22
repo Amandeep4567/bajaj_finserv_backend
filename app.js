97a0081b4944b794b64392aedf0fbd0093a0a4ee
Amandeep4567 <amankumaradk@gmail.com>
2024-09-22 20:09:59 +0530

    commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "https://bajaj-challenge-frontend-psi.vercel.app/",
+  origin: "http://localhost:3000",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"],
 };
